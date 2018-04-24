7376bab44e751e239be423353e622c57a0711af5
Rohith Reddy Kumbharkar <rohith@users.noreply.github.com>
2018-04-24 17:03:38 +0530

    Init with Web Extension directory structure

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require("gulp"),
+    babel = require("gulp-babel"),
+    stylus = require("gulp-stylus"),
+    del = require("del")
+
+gulp.task("clean", function () {
+    return del(["dist"])
+})
+
+gulp.task("styles", function () {
+    return gulp.src("src/*.styl")
+        .pipe(stylus())
+        .pipe(gulp.dest("dist"))
+})
+
+gulp.task("scripts", function () {
+    return gulp.src("src/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("dist"))
+})
+
+gulp.task("copy", function () {
+    return gulp.src(["src/manifest.json", "src/*.html"])
+        .pipe(gulp.dest("dist"))
+})
+
+gulp.task("watch", function () {
+    gulp.watch("src/*.styl", ["styles"])
+    gulp.watch("src/*.js", ["scripts"])
+    gulp.watch(["src/manifest.json", "src/*.html"], ["copy"])
+})
+
+gulp.task("default", ["clean", "styles", "scripts", "copy", "watch"])
